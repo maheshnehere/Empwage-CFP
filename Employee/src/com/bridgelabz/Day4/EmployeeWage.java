3466a0aea39ab590fcff82fb7258cf72f36bcaba
maheshnehere <maheshnehere93@gmail.com>
2024-02-15 01:16:09 +0530

    condition for wages

--- Employee/src/com/bridgelabz/Day4/EmployeeWage.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWage.java
@@ -5,26 +5,36 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-
-        int FullTimeHrs = 8,partTimeHrs = 4,wagePerHour = 20;
-        int workingDaysInMonth = 20,wageForMonth, wageForMonthPartTime;
-
-        wageForMonth = FullTimeHrs * wagePerHour * workingDaysInMonth;
-
-        wageForMonthPartTime = partTimeHrs * wagePerHour * workingDaysInMonth;
+        int fullTimeHours = 8;
+        int partTimeHours = 4;
+        int wagePerHour = 20;
+        int fullTimeWageForMonth, partTimeWageForMonth;
 
         Random random = new Random() ;
+        //Number of Working Days.
+
+        int num1 = random.nextInt(20)+1;
+        System.out.println("Numbers of Total Working Days in Month =" + num1);
+
+        int workingDaysInMonth = num1 ;
+
+
+        fullTimeWageForMonth = fullTimeHours * wagePerHour * workingDaysInMonth;
+        partTimeWageForMonth = partTimeHours * wagePerHour * workingDaysInMonth;
+
+
         int num = random.nextInt(3);
 
         switch (num){
             case(0) :
-                System.out.println("Full time Employee Wage for Month is = " + wageForMonth);
+                System.out.println("Full time Employee Wage for Month is = " + fullTimeWageForMonth);
                 break;
             case(2) :
-                System.out.println("Part time Employee Wage for Month is = " + wageForMonthPartTime);
+                System.out.println("Part time Employee Wage for Month is = " + partTimeWageForMonth);
                 break;
             default:
                 System.out.println("Employee is Absent");
+
         }
     }
 }
