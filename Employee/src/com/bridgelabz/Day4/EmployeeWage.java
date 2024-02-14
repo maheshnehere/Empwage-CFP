e105ac9a7d362e0591df4c43ec8f1b5777fbb008
maheshnehere <maheshnehere93@gmail.com>
2024-02-15 00:47:08 +0530

    Employee attendance

--- Employee/src/com/bridgelabz/Day4/EmployeeWage.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWage.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.Day4;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        // UC 1 = Check Employee is Present or Absent
+        Random random = new Random();
+        boolean num = random.nextBoolean();
+
+        if (num == true) {
+            System.out.println(" Employee is Present");
+        } else {
+            System.out.println(" Employee is Absent");
+        }
     }
 }
