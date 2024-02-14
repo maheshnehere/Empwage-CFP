7ef132a616634de07111e9d1f2110ed8aa35c470
maheshnehere <maheshnehere93@gmail.com>
2024-02-15 00:53:38 +0530

    Employee daily wages

--- Employee/src/com/bridgelabz/Day4/EmployeeWage.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWage.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.Day4;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int fullDayHours = 8, wagePerHour = 20,dailyWage = 0;
+
+        dailyWage = fullDayHours * wagePerHour;
+
+        Random random = new Random();
+        boolean num = random.nextBoolean();
+
+        if (num) { // Employee is present.
+            System.out.println("Daily Employee wage is = " + dailyWage);
+        } else { // Employee is absent.
+            System.out.println("Daily Employee wage is = 0");
+        }
     }
 }
