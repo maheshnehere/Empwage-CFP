a1a845d5032b52addcecb1199d809f766325a1b8
maheshnehere <maheshnehere93@gmail.com>
2024-02-15 01:07:33 +0530

    part time wage

--- Employee/src/com/bridgelabz/Day4/EmployeeWage.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWage.java
@@ -5,17 +5,22 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int fullDayHours = 8, wagePerHour = 20,dailyWage = 0;
 
-        dailyWage = fullDayHours * wagePerHour;
+        int partTime = 4,FullTimeHrs = 8,wagePerHour = 20,partTimeWage = 0,wageFullTime = 0;
+
+        partTimeWage = partTime * wagePerHour;
+        wageFullTime = FullTimeHrs * wagePerHour;
+
 
         Random random = new Random();
-        boolean num = random.nextBoolean();
+        int num = random.nextInt(3);
 
-        if (num) { // Employee is present.
-            System.out.println("Daily Employee wage is = " + dailyWage);
-        } else { // Employee is absent.
-            System.out.println("Daily Employee wage is = 0");
+        if (num == 0) { // Employee is present.
+            System.out.println("Part time Employee Wage  : " + partTimeWage);
+        } else if(num == 1){ // Employee is absent.
+            System.out.println("Full time Employee Wage  : "+wageFullTime);
+        }else {
+            System.out.println("Employee is Absent");
         }
     }
 }
