42272e961be2645c5be4ac8ef678c6eafa720b0f
maheshnehere <maheshnehere93@gmail.com>
2024-02-26 12:55:14 +0530

    emp wage calculation of multiple companies

--- Employee/src/com/bridgelabz/Day4/EmployeeWageMain.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWageMain.java
@@ -3,16 +3,14 @@ package com.bridgelabz.Day4;
 
     public class EmployeeWageMain {
         public static void main(String[] args) {
-            // Creating instances of EmpWageBuilder for each company
-            EmployeeWage companyA = new EmployeeWage("Company A", 20, 22, 100);
-            EmployeeWage companyB = new EmployeeWage("Company B", 25, 25, 120);
+            // Creating an instance of EmpWageBuilder
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+
+            // Adding companies and their respective parameters
+            empWageBuilder.addCompanyEmpWage("Company A", 20, 22, 100);
+            empWageBuilder.addCompanyEmpWage("Company B", 25, 25, 120);
 
             // Computing employee wage for each company
-            companyA.computeEmployeeWage();
-            companyB.computeEmployeeWage();
-
-            // Displaying total employee wage for each company
-            companyA.displayTotalEmpWage();
-            companyB.displayTotalEmpWage();
+            empWageBuilder.computeEmpWage();
         }
 }
