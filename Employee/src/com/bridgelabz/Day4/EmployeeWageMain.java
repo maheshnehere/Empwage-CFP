9397a82f73515be191eb1dd80671af57fd2703e4
maheshnehere <maheshnehere93@gmail.com>
2024-02-26 16:02:09 +0530

    Calculate total wage when queried

--- Employee/src/com/bridgelabz/Day4/EmployeeWageMain.java
+++ Employee/src/com/bridgelabz/Day4/EmployeeWageMain.java
@@ -10,6 +10,10 @@ package com.bridgelabz.Day4;
                 empWageBuilder.addCompanyEmpWage("Company A", 20, 22, 100);
                 empWageBuilder.addCompanyEmpWage("Company B", 25, 25, 120);
 
+                String companyNameToQuery = "Company A";
+                int totalWageByCompany = empWageBuilder.getTotalWageByCompany(companyNameToQuery);
+                System.out.println("Total Wage for " + companyNameToQuery + ": $" + totalWageByCompany);
+
                 // Computing employee wage for each company
                 empWageBuilder.computeEmpWage();
             }
